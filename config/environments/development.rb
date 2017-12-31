03d7db5ee778424abe1da89d94a37a656d252238
Matt Neal <findme@mattneal.com.au>
2017-12-31 13:37:55 +1000

    Setup Stripe keys for development, test and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Read secrets in development
+  config.read_encrypted_secrets = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
